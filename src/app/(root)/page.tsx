a6e1171e02d95a6a07bee935286ce4bb5c354d18
vidit <viditsharma818@gmail.com>
2024-07-26 09:42:30 +0530

    drag and drop feature

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,13 +3,27 @@ import Sidebar from "@/components/common/sidebar";
 import { RecordVoiceOver, StopCircleOutlined } from "@mui/icons-material";
 import PauseCircleIcon from "@mui/icons-material/PauseCircle";
 import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import TextField from "@mui/material/TextField";
 import axios from "axios";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useDropzone } from "react-dropzone";
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
   const [isRecording, setIsRecording] = useState<boolean>(false);
   const [isPaused, setIsPaused] = useState<boolean>(false);
   const [audioBlob, setAudioBlob] = useState<Blob | null>(null);
+  const [isDragActive, setIsDragActive] = useState<boolean>(false);
+  const [file, setFile] = useState<File | null>(null);
+  const [patientName, setPatientName] = useState<string>("");
+  const [dob, setDob] = useState<string>("");
+
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
   const audioChunksRef = useRef<Blob[]>([]);
   const audioContextRef = useRef<AudioContext | null>(null);
@@ -132,87 +146,184 @@ export default function Home() {
     }
   };
 
-  const handleGenerateNote = async () => {
-    if (!audioBlob) return;
-    try {
-      const reader = new FileReader();
-      reader.readAsDataURL(audioBlob);
-      reader.onloadend = async () => {
-        const base64Audio = reader.result as string;
-
-        const response = await axios.post("/api/transcribe", {
-          audioBlob: base64Audio,
-        });
-        console.log("response===>", response);
-      };
-    } catch (error) {
-      console.error("Error generating note:", error);
-    }
+  const handleDialogOpen = () => {
+    setOpen(true);
   };
+
+  const handleDialogClose = () => {
+    setOpen(false);
+  };
+
+  const handleGenerateNote = async (skip: boolean = false) => {
+    if (!audioBlob && !file) return;
+    const reader = new FileReader();
+    const blob = audioBlob || file;
+    reader.readAsDataURL(blob!);
+    reader.onloadend = async () => {
+      const base64Audio = reader.result as string;
+      const payload = {
+        audioBlob: base64Audio,
+        patientName: skip ? "" : patientName,
+        dob: skip ? "" : dob,
+      };
+      try {
+        const response = await axios.post("/api/transcribe", payload);
+        if (response.data) {
+          setFile(null);
+          setAudioBlob(null);
+        }
+        console.log("Response:", response.data);
+      } catch (error) {
+        console.error("Error generating note:", error);
+      }
+      handleDialogClose();
+    };
+  };
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    const file = acceptedFiles[0];
+    setFile(file);
+    handleDialogOpen();
+  }, []);
+
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive: dropzoneIsActive,
+  } = useDropzone({
+    noClick: true,
+    onDrop,
+    onDragEnter: () => setIsDragActive(true),
+    onDragLeave: () => setIsDragActive(false),
+  });
+
+  useEffect(() => {
+    if (!dropzoneIsActive) {
+      setIsDragActive(false);
+    }
+  }, [dropzoneIsActive]);
 
   return (
     <div
-      className="h-screen flex relative w-full bg-white"
+      {...getRootProps()}
+      className="relative h-screen w-full"
       style={{ height: "calc(100vh - 64px)" }}
     >
-      <Sidebar />
-      <div className="flex flex-col items-center justify-center w-full relative">
-        <canvas
-          ref={canvasRef}
-          width={340}
-          height={140}
-          className="border mt-8"
-        ></canvas>
-        <div className="mb-8 flex mt-4">
-          <button
-            onClick={handlePlayPause}
-            className="w-24 h-24 bg-primary-500 rounded-full flex items-center justify-center shadow-lg hover:bg-blue-600 transition-colors duration-300 z-50"
-          >
-            {isRecording ? (
-              isPaused ? (
-                <PlayCircleIcon
-                  style={{ fontSize: 50 }}
-                  className="text-white"
-                />
-              ) : (
-                <PauseCircleIcon
-                  style={{ fontSize: 50 }}
-                  className="text-white"
-                />
-              )
-            ) : (
-              <RecordVoiceOver
-                style={{ fontSize: 50 }}
-                className="text-white"
-              />
-            )}
-          </button>
-          {isRecording && (
-            <button
-              onClick={stopRecording}
-              className="w-24 h-24 bg-red-500 rounded-full flex items-center justify-center shadow-lg hover:bg-red-700 transition-colors duration-300 z-50 ml-4"
+      <input {...getInputProps()} />
+      <div className=" flex relative w-full h-full bg-white">
+        <Sidebar />
+        <div className="flex flex-col items-center justify-center w-full relative">
+          {isDragActive && (
+            <div
+              className="absolute p-2 top-0 left-0 w-full h-full flex items-end justify-center"
+              style={{ zIndex: 9999 }}
             >
-              <StopCircleOutlined
-                style={{ fontSize: 50 }}
-                className="text-white"
-              />
-            </button>
+              <div className="bg-black p-2 bg-opacity-10 border-2 border-dotted border-cyan-500 rounded-lg w-full h-full flex items-end justify-center">
+                <div className="p-4 bg-[#3489e5] rounded-xl">
+                  <p className="text-white text-sm">
+                    Drop audio files here to upload them
+                  </p>
+                </div>
+              </div>
+            </div>
           )}
+          <div className="mb-8 flex">
+            <button
+              onClick={handlePlayPause}
+              className="w-24 h-24 bg-primary-500 rounded-full flex items-center justify-center shadow-lg hover:bg-blue-600 transition-colors duration-300 z-50"
+            >
+              {isRecording ? (
+                isPaused ? (
+                  <PlayCircleIcon
+                    style={{ fontSize: 50 }}
+                    className="text-white"
+                  />
+                ) : (
+                  <PauseCircleIcon
+                    style={{ fontSize: 50 }}
+                    className="text-white"
+                  />
+                )
+              ) : (
+                <RecordVoiceOver
+                  style={{ fontSize: 50 }}
+                  className="text-white"
+                />
+              )}
+            </button>
+            {isRecording && (
+              <button
+                onClick={stopRecording}
+                className="w-24 h-24 bg-red-500 rounded-full flex items-center justify-center shadow-lg hover:bg-red-700 transition-colors duration-300 z-50 ml-4"
+              >
+                <StopCircleOutlined
+                  style={{ fontSize: 50 }}
+                  className="text-white"
+                />
+              </button>
+            )}
+          </div>
+          <p className="text-gray-700 mb-6">
+            Press &apos;Record&apos; to capture the visit
+          </p>
+          {audioBlob && (
+            <audio controls>
+              <source src={URL.createObjectURL(audioBlob)} type="audio/wav" />
+            </audio>
+          )}
+          <button
+            onClick={handleDialogOpen}
+            className="px-6 py-2 bg-primary-500 text-white rounded-md shadow hover:bg-blue-600 transition-colors duration-300 z-50"
+          >
+            Generate Note
+          </button>
+          <canvas
+            ref={canvasRef}
+            width={640}
+            height={240}
+            className="border mt-8"
+          ></canvas>
+          <Dialog open={open} onClose={handleDialogClose}>
+            <DialogTitle>Patient Information</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Please enter the patient&apos;s name and date of birth.
+              </DialogContentText>
+              <TextField
+                autoFocus
+                required
+                margin="dense"
+                id="name"
+                label="Patient Name"
+                type="text"
+                fullWidth
+                variant="standard"
+                value={patientName}
+                onChange={(e) => setPatientName(e.target.value)}
+              />
+              <TextField
+                required
+                margin="dense"
+                id="dob"
+                label="Date of Birth"
+                type="date"
+                fullWidth
+                variant="standard"
+                value={dob}
+                onChange={(e) => setDob(e.target.value)}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={() => handleGenerateNote(true)}>Skip</Button>
+              <Button onClick={() => handleGenerateNote(false)}>
+                Generate
+              </Button>
+            </DialogActions>
+          </Dialog>
         </div>
-        <p className="text-gray-700 mb-6">
-          Press &apos;Record&apos; to capture the visit
-        </p>
-        {audioBlob && (
-          <audio controls>
-            <source src={URL.createObjectURL(audioBlob)} type="audio/wav" />
-          </audio>
-        )}
-        <button
-          onClick={handleGenerateNote}
-          className="px-6 py-2 bg-primary-500 text-white rounded-md shadow hover:bg-blue-600 transition-colors duration-300 z-50"
-        >
-          Generate Note
-        </button>
       </div>
     </div>
   );
--- src/app/api/transcribe/route.ts
+++ src/app/api/transcribe/route.ts
@@ -5,40 +5,52 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
   try {
-    const { audioBlob } = await req.json();
-    if (!audioBlob) {
+    const contentType = req.headers.get("content-type");
+
+    if (contentType?.includes("application/json")) {
+      // Handle audioBlob
+      const { audioBlob, patientName, dob } = await req.json();
+      console.log("patientName===>", patientName);
+      console.log("dob===>", dob);
+      if (!audioBlob) {
+        return NextResponse.json(
+          { error: "No audioBlob found" },
+          { status: 400 }
+        );
+      }
+
+      try {
+        const buffer = Buffer.from(audioBlob.split(",")[1], "base64");
+
+        const formData = new FormData();
+        formData.append("file", buffer, {
+          filename: "audio.wav",
+          contentType: "audio/wav",
+        });
+        const response = await axios.post(
+          `${env_config.azure_endpoint}/openai/deployments/${env_config.azure_deployment_name}/audio/transcriptions?api-version=2024-02-01`,
+          formData,
+          {
+            headers: {
+              ...formData.getHeaders(),
+              "api-key": env_config.openai_key,
+            },
+          }
+        );
+
+        console.log("response===>", response.data);
+        return NextResponse.json(response.data);
+      } catch (error: any) {
+        console.error(error.response?.data || error.message);
+        return NextResponse.json(
+          { error: "Error creating transcription in azure server" },
+          { status: 500 }
+        );
+      }
+    } else {
       return NextResponse.json(
-        { error: "No audio file found" },
+        { error: "Unsupported content type" },
         { status: 400 }
-      );
-    }
-
-    try {
-      const buffer = Buffer.from(audioBlob.split(",")[1], "base64");
-
-      const formData = new FormData();
-      formData.append("file", buffer, {
-        filename: "audio.wav",
-        contentType: "audio/wav",
-      });
-      const response = await axios.post(
-        `${env_config.azure_endpoint}/openai/deployments/${env_config.azure_deployment_name}/audio/transcriptions?api-version=2024-02-01`,
-        formData,
-        {
-          headers: {
-            ...formData.getHeaders(),
-            "api-key": env_config.openai_key,
-          },
-        }
-      );
-
-      console.log("response===>", response.data);
-      return NextResponse.json(response.data);
-    } catch (error: any) {
-      console.error(error.response?.data || error.message);
-      return NextResponse.json(
-        { error: "Error creating transcription in azure server" },
-        { status: 500 }
       );
     }
   } catch (error) {
