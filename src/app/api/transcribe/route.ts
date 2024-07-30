8be51a7feb034611283176b2e47ab9b1b87c8e68
vidit <viditsharma818@gmail.com>
2024-07-30 19:35:12 +0530

    readstream update

--- src/app/api/transcribe/route.ts
+++ src/app/api/transcribe/route.ts
@@ -3,104 +3,6 @@ import axios from "axios";
 import FormData from "form-data";
 import { NextRequest } from "next/server";
 
-// export const POST = async (req: NextRequest) => {
-//   try {
-//     const contentType = req.headers.get("content-type");
-
-//     if (contentType?.includes("application/json")) {
-//       // Handle audioBlob
-//       const { audioBlob, patientName, dob } = await req.json();
-//       console.log("patientName===>", patientName);
-//       console.log("dob===>", dob);
-//       if (!audioBlob) {
-//         return NextResponse.json(
-//           { error: "No audioBlob found" },
-//           { status: 400 }
-//         );
-//       }
-
-//       try {
-//         const buffer = Buffer.from(audioBlob.split(",")[1], "base64");
-
-//         const formData = new FormData();
-//         formData.append("file", buffer, {
-//           filename: "audio.wav",
-//           contentType: "audio/wav",
-//         });
-//         const response = await axios.post(
-//           `${env_config.azure_endpoint}/openai/deployments/${env_config.azure_deployment_name}/audio/transcriptions?api-version=2024-02-01`,
-//           formData,
-//           {
-//             headers: {
-//               ...formData.getHeaders(),
-//               "api-key": env_config.openai_key,
-//             },
-//           }
-//         );
-
-//         console.log("response===>", response.data);
-//         const transcription = response.data.text;
-
-//         // caling the api to genrate the note
-
-//         const completionResponse: any = await axios.post(
-//           `${env_config.azure_endpoint}/openai/deployments/eka-gpt4o/chat/completions?api-version=2024-04-01-preview`,
-//           {
-//             messages: [
-//               {
-//                 role: "system",
-//                 content: [
-//                   {
-//                     type: "text",
-//                     text: 'You are an AI assistant designed to assist doctors by transcribing and summarizing medical conversations. Given a conversation between a doctor and a patient, your task is to produce a structured medical note in the following JSON format:\n\n{\n    "Doctor_Patient_Discussion": {\n        "Initial_Observation": {\n            "Symptoms": [],\n            "Initial_Assessment": ""\n        },\n        "Medical_Examination": {\n            "Temperature":"",\n            "Blood_Pressure":"",\n            "Doctor_Assessment": "",\n            "Diagnosis": ""\n        },\n        "Treatment_Plan": {\n            "Prescription": []\n        }\n    }\n}\n\nPlease ensure the notes are detailed and accurate based on the conversation provided.',
-//                   },
-//                 ],
-//               },
-//               {
-//                 role: "user",
-//                 content: [
-//                   {
-//                     type: "text",
-//                     text: "Hello Mr. Johnson. What brings you here today?Hi Doctor Strange. I haven't been feeling too well. I think I might have the flu. I'm so sorry to hear that. When did it first start?Two days ago.It definitely looks like the flu. I recommend that you drink lots of fluids, rest, and take Ibuprofen if you experience any sore throat or headaches. Then let's have you come back in 2 weeks and see how you're doing. Does that sound good?That sounds great doctor. Thank you for seeing me today.Goodbye",
-//                   },
-//                 ],
-//               },
-//             ],
-//           },
-//           {
-//             headers: {
-//               "Content-Type": "application/json",
-//               "api-key": env_config.openai_key,
-//             },
-//           }
-//         );
-//         console.log("completionResponse===>", completionResponse);
-
-//         return NextResponse.json({
-//           Notes: completionResponse.data.choices[0].message.content,
-//         });
-//       } catch (error: any) {
-//         console.error(error.response?.data || error.message);
-//         return NextResponse.json(
-//           { error: "Error creating transcription in azure server" },
-//           { status: 500 }
-//         );
-//       }
-//     } else {
-//       return NextResponse.json(
-//         { error: "Unsupported content type" },
-//         { status: 400 }
-//       );
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     return NextResponse.json(
-//       { error: "Error creating transcription" },
-//       { status: 500 }
-//     );
-//   }
-// };
-
 export const POST = async (req: NextRequest) => {
   const encoder = new TextEncoder();
 
@@ -151,7 +53,7 @@ export const POST = async (req: NextRequest) => {
             );
 
             // Chat completion API call
-            const completionResponse: any = await axios.post(
+            const completionResponse = await axios.post(
               `${env_config.azure_endpoint}/openai/deployments/eka-gpt4o/chat/completions?api-version=2024-04-01-preview`,
               {
                 messages: [
